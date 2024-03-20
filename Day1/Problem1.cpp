c6cca4edc7f02cf88e45f8bf84e063ea19c007cf
Mirza Mehedi Abbas <140306222+mmabbas786@users.noreply.github.com>
2024-03-20 19:21:42 +0000

    Rudolf And 121 Solved

---
+++ Day1/Problem1.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    int a[n];
+
+    for(int i=0;i<n;i++)
+    {
+        cin >> a[i];
+    }
+
+    bool flag = true;
+    for(int i=1;i<n;i++)
+    {
+        a[i]-=2*a[i-1];
+        a[i+1]-=a[i-1];
+        a[i-1]=0;
+        if(a[i]<0 || a[i+1]<0)
+        {
+            flag = false;
+            break;
+        }
+    }
+
+    if(flag && a[n-1]==0 && a[n-2]==0)
+    {
+        cout<<"YES\n";
+        return;
+    }
+    cout<<"NO\n";
+}
+
+int main(){
+    int t;
+    cin >> t; // Input number of test cases
+
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
